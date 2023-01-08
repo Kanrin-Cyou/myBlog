ca4f880d8e34d7963046398ddf317a030e91de05
Guanlun <zhaoguanlun@gmail.com>
2023-01-08 22:14:32 +0900

    fix theme bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)} style={{background: "linear-gradient(135deg,#00d2ff,#3a7bd5)"}}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <h1 className="hero__title" style={{color: "#fff"}}>{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
